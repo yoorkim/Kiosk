5ab242784ec318256e6ef56624c8a580d60df6fc
yoorkim <amy0848@naver.com>
2025-01-14 15:52:17 +0900

    level5 구현

---
+++ src/main/java/com/example/level5/Menu.java
@@ -0,0 +1,34 @@
+package com.example.level5;
+
+import java.util.List;
+
+public class Menu {
+    private String category;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String category, List<MenuItem> menuItems) {
+        this.category = category;
+        this.menuItems = menuItems;
+    }
+
+    // 메뉴 카테고리 이름 반환 메서드
+    public String getCategory() {
+        return category;
+    }
+    // MenuItem List를 반환하는 메서드
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+    // 카테고리 이름 변경 메서드
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    // List에 들어있는 MenuItem을 순차적으로 보여주는 메서드
+    public void printMenuItems() {
+        for (int i = 0; i < menuItems.size(); i++) {
+            String menu = String.format("%-16s", menuItems.get(i).getMenu());  // 문자열 간격 맞추기
+            System.out.println((i+1) + ". " + menu + "| W "
+                    + menuItems.get(i).getPrice() + " | " + menuItems.get(i).getMenuInfo());
+        }
+    }
+}
