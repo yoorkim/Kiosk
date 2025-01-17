f6600fb1b0235ded7166a98ef814480ec824d698
yoorkim <amy0848@naver.com>
2025-01-17 16:18:50 +0900

    문자열 정렬 변수 삭제

--- src/main/java/com/example/level4/Menu.java
+++ src/main/java/com/example/level4/Menu.java
@@ -22,8 +22,7 @@ public class Menu {
     // List에 들어있는 MenuItem을 순차적으로 보여주는 메서드
     public void printMenuItems() {
         for (int i = 0; i < menuItems.size(); i++) {
-            String menu = String.format("%-16s", menuItems.get(i).menu);  // 문자열 간격 맞추기
-            System.out.printf("%d. %s| W %.2f | %s\n", i+1, menu, menuItems.get(i).price, menuItems.get(i).menuInfo);
+            System.out.printf("%d. %-16s| W %.2f | %s\n", i+1, menuItems.get(i).menu, menuItems.get(i).price, menuItems.get(i).menuInfo);
         }
     }
 }
